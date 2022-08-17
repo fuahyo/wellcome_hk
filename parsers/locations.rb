bf0a993e9f85d006e9e8d42d56add02135bbf99a
Kadda GHOUADNI <ghouadni55@hotmail.fr>
2022-08-18 00:41:53 +0100

    Update scraper.

--- parsers/locations.rb
+++ parsers/locations.rb
@@ -1,11 +1,22 @@
 require './lib/headers'
 
-body = 'param={"wifiList":[{"signal":0}],"flowDeliveryTimeType":"1","onlineShowType":1,"onlineBizCode":1,"deliveryLocation":{"longitude":114.1693611,"latitude":22.319303900000001}}'
+html=Nokogiri::HTML(content)
 
-pages << {
-    page_type: "stores",
-    url: "https://flow.dmall.com.hk/app/home/business",
-    method: "POST",
-    body: body,
-    headers: ReqHeaders::HEADERS,
-}
+locations = html.css('.store-title')
+
+locations.each do |location|
+
+  coordinates = location.attr('data-latlng')
+  lat,lng = coordinates.split("|")
+  body = 'param={"wifiList":[{"signal":0}],"flowDeliveryTimeType":"1","onlineShowType":1,"onlineBizCode":1,"deliveryLocation":{"longitude":'+lat+',"latitude":'+lng+'}}'
+
+  pages << {
+      page_type: "stores",
+      url: "https://flow.dmall.com.hk/app/home/business",
+      method: "POST",
+      body: body,
+      headers: ReqHeaders::HEADERS,
+  }
+
+end
+
