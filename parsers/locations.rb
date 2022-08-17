a4b91a2e361dcf7b6fc8dadf1db46cf241fffb5b
Kadda GHOUADNI <ghouadni55@hotmail.fr>
2022-08-18 00:35:02 +0100

    Update scraper.

---
+++ parsers/locations.rb
@@ -0,0 +1,11 @@
+require './lib/headers'
+
+body = 'param={"wifiList":[{"signal":0}],"flowDeliveryTimeType":"1","onlineShowType":1,"onlineBizCode":1,"deliveryLocation":{"longitude":114.1693611,"latitude":22.319303900000001}}'
+
+pages << {
+    page_type: "stores",
+    url: "https://flow.dmall.com.hk/app/home/business",
+    method: "POST",
+    body: body,
+    headers: ReqHeaders::HEADERS,
+}
