932cca49bb5b24cf5971ab4f06e0f784c66a9b36
bunxbio <biokenfigumilang@gmail.com>
2023-08-19 01:45:45 +0700

    enabling categories parser

--- parsers/categories.rb
+++ parsers/categories.rb
@@ -33,17 +33,38 @@ cat1.each do |i|
     cat_id = i["categoryId"]
     cat_name = i["categoryName"]
 
-    subcat = []
-
     cat2 = i["childCategoryList"]
     cat2.each do |j|
-        subcat.append("#{j["categoryName"]}")
 
         cat3 = j["childCategoryList"]
         cat3.each do |k|
+            subcat = []
+            subcat.append("#{j["categoryName"]}")
             subcat.append("#{k["categoryName"]}")
+            subcat = subcat.join(" > ")
+
+            last_subcat_id = k["categoryId"]
+            last_subcat_lvl = k["categoryLevel"]
+            last_subcat_type = k["categoryType"]
+
+            #body = 'param={"businessCode":1,"categoryId":"100849","categoryLevel":3,"categorySkuId":0,"categoryType":1,"erpStoreId":"551004","filterProperties":[],"from":1,"globalSelection":false,"noResultSearch":0,"pos":1,"promoting":0,"sortKey":0,"sortRule":0,"src":0,"venderId":"5","pageNum":"1","pageSize":"20"}'
+            body = "param=%7B%22businessCode%22%3A1%2C%22categoryId%22%3A%22#{last_subcat_id}%22%2C%22categoryLevel%22%3A#{last_subcat_lvl}%2C%22categorySkuId%22%3A0%2C%22categoryType%22%3A#{last_subcat_type}%2C%22erpStoreId%22%3A%22551004%22%2C%22filterProperties%22%3A%5B%5D%2C%22from%22%3A1%2C%22globalSelection%22%3Afalse%2C%22noResultSearch%22%3A0%2C%22pos%22%3A1%2C%22promoting%22%3A0%2C%22sortKey%22%3A0%2C%22sortRule%22%3A0%2C%22src%22%3A0%2C%22venderId%22%3A%225%22%2C%22pageNum%22%3A%221%22%2C%22pageSize%22%3A%2220%22%7D"
+
+            pages << {
+                page_type: "listings",
+                url: "https://searchgw.rta-os.com/app/search/wareSearch",
+                method: "POST",
+                priority: 100,
+                body: body,
+                headers: ReqHeaders::AllHeaders,
+                vars: vars.merge(
+                    "cat_id" => cat_id,
+                    "cat_name" => cat_name,
+                    "subcat" => subcat,
+                )
+            }
+
+            save_pages pages if pages.count > 99
         end
     end
 end
-
-puts subcat
