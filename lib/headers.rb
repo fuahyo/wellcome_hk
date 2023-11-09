a3620b5ac0cde92672a65e5ce79085bb27719bdf
LitvinenkoD89 <torerolda@gmail.com>
2023-11-09 20:44:27 +0300

    update headers and request body

--- lib/headers.rb
+++ lib/headers.rb
@@ -4,7 +4,7 @@ module ReqHeaders
 	AllHeaders = {
 		"accept-encoding" => "gzip",
 		#an_memberid:934480046302576
-		"apiversion" => "5.33.0",
+		"apiversion" => "'6.3.5",
 		#appmode:online
 		#appname:com.rtasia.intl
 		"businesscode" => "1",
@@ -35,14 +35,14 @@ module ReqHeaders
 		#sdkversion:1.33.2
 		#sessionid:a58a2599bb0d43bfaba05f00ce1306f0
 		#smartloading:1
-		"storegroupv4" => "1-551004-5",
-		"storeid" => "551004",
+		"storegroupv4" => "1-43-5",
+		"storeid" => "43",
 		#sysversion:Android-8.0.0
 		#tdc:
 		#ticketname:8D6D86F014E2222056CC1B1E42E3609928590623C4426240CEB0807CB8D9C3CE94C86EF6B62F022C96450473D1D3BC10B3721ED197A9016E1C82AE6FB20733E94104CCA11EEDC8143927EDEB32154F89777E8BEFFC5783229FB6F6BBB5BC35A018C319A0514C79B2CC51A65BD699CDC95A3D7EF1D2717BCE7B641BE8837B5CDE
 		#token:d82b5b95-8296-4dcc-a7ce-761b760ead9d
 		#tpc:
-		"user-agent" => "dmall/1.33.2 Dalvik/2.1.0 (Linux; U; Android 8.0.0; Galaxy S10 Build/OPR6.170623.017)",
+		"user-agent" => "dmall/2.4.0 Dalvik/2.1.0 (Linux; U; Android 11; Nexus 4 Build/RQ1A.210105.003)",
 		#userid:205627765
 		#utmid:
 		#utmsource:
