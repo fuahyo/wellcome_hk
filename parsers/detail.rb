f8279f7f399486ea91ab01017978895c0a780c0c
Galang Dwi Juniar <99375881+fuahyo@users.noreply.github.com>
2024-08-01 17:12:54 +0700

    Update detail.rb

--- parsers/detail.rb
+++ parsers/detail.rb
@@ -1,3 +1,17 @@
 vars = page["vars"]
 html = Nokogiri::HTML(content)
 
+list_promo_attributes = []
+html.css('.offer-item').each do |item|
+  promo_text = item.at_css('.info-content .tag')&.text&.strip
+  list_promo_attributes << promo_text if promo_text
+end
+
+promo_attributes = list_promo_attributes.join(', ') 
+if vars['promo_attributes'] == "{\"promo_details\":\"\"}" 
+  vars['promo_attributes'] = "{\"promo_details\":\""+(promo_attributes || "")+"\"}"
+  vars['type_of_promotion'] = 'Offers' if !promo_attributes.nil?
+  vars['is_promoted'] = !promo_attributes.nil?
+end
+ 
+output << vars
